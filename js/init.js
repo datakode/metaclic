7594d30bbb98ff7eb8113d31aa2366f1b03bec3e
Caperan Florian <caperan@datakode.fr>
2016-12-08 16:23:51 +0100

    reorganisation ajout fichiers oubliés

---
+++ js/init.js
@@ -0,0 +1,2 @@
+var Metaclic;
+var MetaclicUtils = {};
---
+++ js/params.js
@@ -0,0 +1,74 @@
+MetaclicUtils.baseLayers = {
+    "OSM-Fr": {
+        "title": "OSM-Fr",
+        "url": "https://tilecache.openstreetmap.fr/osmfr/{z}/{x}/{y}.png"
+    },
+    "Positron": {
+        "title": "Positron",
+        "url": "https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
+    },
+    "Outdoors_OSM": {
+        "title": "Outdoors (OSM)",
+        "url": "https://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png"
+    },
+    "OSM_Roads": {
+        "title": "OSM Roads",
+        "url": "https://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}"
+    },
+    "Dark_Matter": {
+        "title": "Dark Matter",
+        "url": "https://cartodb-basemaps-a.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+    },
+    "OpenStreetMap": {
+        "title": "OpenStreetMap",
+        "url": "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    },
+    "Toner": {
+        "title": "Toner",
+        "url": "https://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png"
+    },
+    "Landscape": {
+        "title": "Landscape",
+        "url": "https://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png"
+    },
+    "Transport": {
+        "title": "Transport",
+        "url": "https://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png"
+    },
+    "MapQuest_Open": {
+        "title": "MapQuest Open",
+        "url": "https://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png"
+    },
+    "HOTOSM_style": {
+        "title": "HOTOSM style",
+        "url": "https://tilecache.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+    },
+    "OpenCycleMap": {
+        "title": "OpenCycleMap",
+        "url": "https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png"
+    },
+    "Watercolor": {
+        "title": "Watercolor",
+        "url": "https://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png"
+    },
+    "hikebikemap": {
+        "title": "hikebikemap",
+        "url": "https://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png"
+    },
+    "OSM-monochrome": {
+        "title": "OSM-monochrome",
+        "url": "https://www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png"
+    },
+    "Hydda": {
+        "title": "Hydda",
+        "url": "https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png"
+    },
+    "OpenTopoMap": {
+        "title": "OpenTopoMap",
+        "url": "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+    },
+    "OpenRiverboatMap": {
+        "title": "OpenRiverboatMap",
+        "url": "https://tilecache.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png"
+    }
+};
